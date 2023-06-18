18b38477746ab6be6065b76842ef2f350d98a28e
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-06-19 00:50:02 +0100

    chore(create-sails): remove template files

--- create-sails/template/config/react/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./views/**/*.ejs", "./assets/js/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
--- create-sails/template/config/tailwind/prettier.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
-  semi: false,
-  singleQuote: true,
-}
