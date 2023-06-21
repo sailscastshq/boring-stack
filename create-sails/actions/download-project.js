7b64ed688c2079a3493e80c6c8d2c572ecee2cd3
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-06-21 19:27:38 +0100

    feat(wip): add degit to download repo

--- create-sails/actions/download-project.js
+++ create-sails/actions/download-project.js
@@ -1,18 +1,17 @@
-import { execSync } from 'child_process'
 import { spinner } from '@clack/prompts'
 import color from 'picocolors'
-
+import degit from 'degit'
 const s = spinner()
+
 export default function downloadProject(projectName, frontend) {
-  const repositoryUrl = 'https://github.com/sailscastshq/boring-stack.git'
-  try {
-    s.start(`Downloading your boring project...`)
-    execSync(
-      `git clone --depth 1 --filter=tree:create-sails ${repositoryUrl} ${projectName}`
-    )
-    s.stop(color.green(`Successfully downloaded your boring project.`))
-  } catch (error) {
-    s.stop(color.red(error.message))
-    process.exit(0)
-  }
+  const emitter = degit(`sailscastshq/boring-stack/${frontend}`)
+  s.start('Downloading your boring project...')
+
+  emitter.on('info', (info) => {
+    console.log(info.message)
+  })
+
+  emitter.clone(`${projectName}`).then(() => {
+    s.stop(color.green('Successfully downloaded your boring project!'))
+  })
 }
