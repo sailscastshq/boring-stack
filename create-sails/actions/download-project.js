8cf1c9d64de5b0dac2b4e69079d0c368d536dbb7
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-06-23 13:04:29 +0100

    feat: add logic to download template with giget

--- create-sails/actions/download-project.js
+++ create-sails/actions/download-project.js
@@ -1,5 +1,36 @@
-export default async function downloadProject(projectName, frontend) {}
+import { downloadTemplate } from 'giget'
+import { cancel } from '@clack/prompts'
+export default async function downloadProject(
+  projectName,
+  { frontend, template }
+) {
+  const templateToCopy = getTemplate(frontend, template)
+  try {
+    const { source, dir } = await downloadTemplate(
+      `github:sailscastshq/boring-stack/templates/${templateToCopy}#develop`,
+      {
+        dir: projectName,
+        repo: 'sailscastshq/boring-stack',
+        ref: 'main',
+        subdir: templateToCopy,
+        force: true,
+        cwd: '.'
+      }
+    )
+    return { source, dir }
+  } catch (error) {
+    if (error instanceof Error) {
+      cancel(error.message)
+      process.exit(1)
+    } else {
+      cancel('Unable to clone template.')
+      process.exit(1)
+    }
+  }
+}
 
-function getTemplate(template = 'mellow', frontend) {
+function getTemplate(frontend, template = 'mellow') {
   return `${template}-${frontend}`
 }
+
+console.log(getTemplate('react', undefined))
