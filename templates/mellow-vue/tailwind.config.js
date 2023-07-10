83111a283cab1f0185526ef6b1ac87b63980b656
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-07-10 20:25:15 +0100

    chore: add brand color

--- templates/mellow-vue/tailwind.config.js
+++ templates/mellow-vue/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./views/**/*.ejs', './assets/js/**/*.{js,vue,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      brand: {
+        DEFAULT: '#6C25C1',
+        50: '#CCAEEF',
+        100: '#C19DEC',
+        200: '#AB7BE6',
+        300: '#9659DF',
+        400: '#8036D9',
+        500: '#6C25C1',
+        600: '#521C92',
+        700: '#371363',
+        800: '#1D0A34',
+        900: '#030105',
+        950: '#000000'
+      }
+    }
   },
   plugins: []
 }
