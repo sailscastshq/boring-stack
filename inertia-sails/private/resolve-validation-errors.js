ede1e0624d48aedd05863fab2bb08db9b1ef3ea3
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-07-20 19:35:31 +0100

    [feat] add support for validation errors (#51)
    * fix: stop sharing an empty object and unnecesarry props
    
    * feat(inertia-sails): implement writing resolveValidationErrors
    
    * feat: populate errors in the errors props

---
+++ inertia-sails/private/resolve-validation-errors.js
@@ -0,0 +1,39 @@
+/**
+ * @module resolveValidationErrors
+ * @description Resolves and formats validation errors from the session for Inertia responses.
+ *
+ * @param {Object} req - The current HTTP request object.
+ * @returns {Object} An object representing the validation errors in the desired format.
+ */
+module.exports = function resolveValidationErrors(req) {
+  if (!req.session || !req.session.errors) {
+    return {}
+  }
+
+  const flashedErrors = req.session.errors
+
+  const collectedErrors = Object.keys(flashedErrors).reduce((result, bag) => {
+    const errorsForBag = flashedErrors[bag]
+    const mappedErrors = errorsForBag.map((error) =>
+      error.replace(/"([^"]+)"/, '$1')
+    )
+    result[bag] = mappedErrors
+    return result
+  }, {})
+
+  const inertiaErrorBag = req.headers['x-inertia-error-bag']
+
+  if (inertiaErrorBag && collectedErrors[inertiaErrorBag]) {
+    const selectedErrors = {
+      [inertiaErrorBag]: collectedErrors[inertiaErrorBag]
+    }
+    return selectedErrors
+  }
+
+  if (collectedErrors.default) {
+    const defaultErrors = { default: collectedErrors.default }
+    return defaultErrors
+  }
+
+  return collectedErrors
+}
