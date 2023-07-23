c8dc5ef3410c2eb3f415729cc82169ac5a74d0a0
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-07-23 12:58:14 +0100

    feat: modify signup flow to send email

---
+++ templates/mellow-vue/api/hooks/mail/index.js
@@ -0,0 +1,17 @@
+/**
+ * mail hook
+ *
+ * @description :: A hook definition.  Extends Sails by adding shadow routes, implicit actions, and/or initialization logic.
+ * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
+ */
+
+module.exports = function defineMailHook(sails) {
+  return {
+    /**
+     * Runs when this Sails app loads/lifts.
+     */
+    initialize: async function () {
+      sails.log.info('Initializing custom hook (`mail`)')
+    }
+  }
+}
