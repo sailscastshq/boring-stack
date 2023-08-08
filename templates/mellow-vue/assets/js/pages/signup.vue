46fe21d2c2a3afe1b89bb5827c3c99b986dd646e
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-08-08 17:20:10 +0100

    feat: remove processing text

--- templates/mellow-vue/assets/js/pages/signup.vue
+++ templates/mellow-vue/assets/js/pages/signup.vue
@@ -299,7 +299,6 @@ const disableSignupButton = computed(() => {
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
           </svg>
-          <span v-if="form.processing"> Processing... </span>
           <span v-else> Sign up </span>
         </button>
       </form>
