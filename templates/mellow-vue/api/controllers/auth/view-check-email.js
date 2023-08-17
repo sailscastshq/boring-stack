1325cef4a852542ddb767d21d83b2b7fe626e6cd
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-08-17 14:54:07 +0100

    fix: typo in message

--- templates/mellow-vue/api/controllers/auth/view-check-email.js
+++ templates/mellow-vue/api/controllers/auth/view-check-email.js
@@ -10,7 +10,7 @@ module.exports = {
   fn: async function () {
     let message = null
     if (this.req.get('referrer').includes('forgot-password')) {
-      message = `We sent a password resent link to ${this.req.session.userEmail}`
+      message = `We sent a password reset link to ${this.req.session.userEmail}`
     } else {
       message = `We sent an email verification link to ${this.req.session.userEmail}`
     }
