b37c780662591ab4de551a05e4d0df996b92d91a
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-08-23 20:57:07 +0100

    chore: clean up types

--- templates/mellow-vue/assets/js/pages/index.vue
+++ templates/mellow-vue/assets/js/pages/index.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { ref } from 'vue'
+
 import { Link, Head, usePage } from '@inertiajs/vue3'
+
 /** @type LoggedInUser */
-let loggedInUser = ref(usePage().props.loggedInUser)
+const loggedInUser = ref(usePage().props.loggedInUser)
 </script>
 
 <template>
