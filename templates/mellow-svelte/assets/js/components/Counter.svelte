e426fb40f5615e28e7919ba4642224328809b650
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-12-25 19:49:16 +0100

    fix(mellow-svelte): upgrade Svelte plugin

--- templates/mellow-svelte/assets/js/components/Counter.svelte
+++ templates/mellow-svelte/assets/js/components/Counter.svelte
@@ -4,7 +4,7 @@
 
 <button
   on:click={() => (count += 1)}
-  class="rounded-lg border border-purple-500 px-4 py-2 text-sm text-gray-600 hover:border-purple-600"
+ class="rounded-lg border border-purple-500 px-4 py-2 text-sm text-gray-600 hover:border-purple-600"
 >
   Clicked {count} times</button
 >
