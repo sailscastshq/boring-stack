ee39cee09803d61c07fe18b1f21181e78f118b02
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-02-29 12:05:13 +0100

    feat(mellow-vue): replace call to sails.inertia.location with the inertiaRedirect custom response

--- templates/mellow-vue/api/controllers/user/logout.js
+++ templates/mellow-vue/api/controllers/user/logout.js
@@ -6,12 +6,14 @@ module.exports = {
   inputs: {},
 
   exits: {
-    success: {}
+    success: {
+      responseType: 'inertiaRedirect'
+    }
   },
 
   fn: async function () {
     sails.inertia.flushShared('loggedInUser')
     delete this.req.session.userId
-    return sails.inertia.location('/')
+    return '/'
   }
 }
