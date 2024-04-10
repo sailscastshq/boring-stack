391d86000c9569511c0dcaeb96509c5b2e1acc9c
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-04-10 18:42:23 +0100

    fix(create-sails-generator): update function to get action name

--- create-sails-generator/tests/utils/get-action-name.test.js
+++ create-sails-generator/tests/utils/get-action-name.test.js
@@ -9,4 +9,10 @@ describe('getActionName', function () {
   it("returns 'user/view-users' when given 'users/index'", function () {
     assert.equal(getActionName('users/index'), 'user/view-users')
   })
+  it("returns 'user/view-user' when given 'users/user'", function () {
+    assert.equal(getActionName('users/user'), 'user/view-user')
+  })
+  it("returns 'user/view-profile' when given 'users/profile'", function () {
+    assert.equal(getActionName('users/profile'), 'user/view-profile')
+  })
 })
