d7e21e56092489d76c76fecb962f0343072037c8
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-09-12 07:28:00 +0100

    feat(mellow-vue): add action to delete profile

---
+++ templates/mellow-vue/api/controllers/user/delete-profile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  friendlyName: 'Delete profile',
+
+  description: '',
+
+  inputs: {},
+
+  exits: {},
+
+  fn: async function (inputs) {
+    // All done.
+    return
+  }
+}
