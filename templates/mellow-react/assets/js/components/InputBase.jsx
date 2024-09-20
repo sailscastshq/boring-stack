a5b7d177ab44bd04f7b0193225effc73f58d05a2
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-09-20 08:35:18 +0100

    feat(mellow-react): add InputBase

--- templates/mellow-react/assets/js/components/InputBase.jsx
+++ templates/mellow-react/assets/js/components/InputBase.jsx
@@ -1,11 +1,13 @@
 export default function InputBase({
+  name,
   value,
   onChange,
   label,
   id,
   icon,
   suffix,
-  ...props
+  type,
+  placeholder
 }) {
   return (
     <div className="relative">
@@ -15,20 +17,20 @@ export default function InputBase({
         </label>
       )}
       <div className="relative">
-        {Icon && (
+        {icon && (
           <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
-            <Icon className="h-5 w-5" />
+            {icon}
           </span>
         )}
         <input
-          ref={ref}
           id={id}
           className={`block w-full rounded-lg border border-gray-300 bg-white py-2 ${
-            Icon ? 'pl-10' : 'pl-3'
-          } pr-3 shadow-sm placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+            icon ? 'pl-10' : 'pl-3'
+          } pr-3 shadow-sm placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500`}
           value={value}
           onChange={onChange}
-          {...props}
+          type={type}
+          placeholder={placeholder}
         />
         {suffix && (
           <span className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
