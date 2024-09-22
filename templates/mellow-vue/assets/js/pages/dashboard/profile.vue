a9ee5b22a91bd637f5d6b48b296cd27351c9a408
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-09-23 00:12:03 +0100

    chore(mellow-vue): add missing extension

--- templates/mellow-vue/assets/js/pages/dashboard/profile.vue
+++ templates/mellow-vue/assets/js/pages/dashboard/profile.vue
@@ -6,7 +6,7 @@ import InputPassword from '@/components/InputPassword.vue'
 import InputEmail from '@/components/InputEmail.vue'
 import InputButton from '@/components/InputButton.vue'
 
-import AppLayout from '@/layouts/AppLayout'
+import AppLayout from '@/layouts/AppLayout.vue'
 
 defineOptions({
   layout: AppLayout
