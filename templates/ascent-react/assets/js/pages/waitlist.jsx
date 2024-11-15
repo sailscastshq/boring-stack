501fe93cec3431726ffbee926067a7e7daf89cc6
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-11-15 22:18:18 +0100

    feat: add basic waitlist page

--- templates/ascent-react/assets/js/pages/waitlist.jsx
+++ templates/ascent-react/assets/js/pages/waitlist.jsx
@@ -1,4 +1,26 @@
-import { Head } from '@inertiajs/react'
+import { Head, useForm } from '@inertiajs/react'
 export default function Waitlist() {
-  return <h1>I am your Waitlist</h1>
+  const { data, setData, ...form } = useForm({ email: '' })
+  function joinWaitlist(e) {
+    e.preventDefault()
+    form.post('/waitlist')
+  }
+  return (
+    <>
+      <Head title="TBJS Ascent" />
+      <header className="mt-12 text-center">
+        <h1 className="text-4xl font-bold text-brand">Ascent</h1>
+        <p>Fictitious tagline as placeholder because why not</p>
+      </header>
+      <form onSubmit={joinWaitlist}>
+        <input
+          type="email"
+          value={data.email}
+          onChange={(e) => setData('email', e.target.value)}
+          placeholder="Enter your email"
+        />
+        <button type="submit">Join Waitlist</button>
+      </form>
+    </>
+  )
 }
