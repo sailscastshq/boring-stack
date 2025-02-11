5445f4f20c86f9eae1fae0c29e83ecd0151d8eda
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-02-11 22:37:36 +0100

    feat(inertia-sails): update hook to include defer and render methods

--- packages/inertia-sails/index.js
+++ packages/inertia-sails/index.js
@@ -4,7 +4,9 @@
  * @description :: A hook definition.  Extends Sails by adding shadow routes, implicit actions, and/or initialization logic.
  * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
  */
-const inertia = require('./private/inertia-middleware')
+const inertia = require('./private/middleware/inertia-middleware')
+const DeferProp = require('./private/props/defer-prop')
+const render = require('./private/render')
 module.exports = function defineInertiaHook(sails) {
   let hook
   const routesToBindInertiaTo = [
@@ -20,7 +22,11 @@ module.exports = function defineInertiaHook(sails) {
     defaults: {
       inertia: {
         rootView: 'app',
-        version: 1
+        version: 1,
+        history: {
+          encrypt: false,
+          clear: false
+        }
       }
     },
     initialize: async function () {
@@ -28,27 +34,80 @@ module.exports = function defineInertiaHook(sails) {
       sails.inertia = hook
       sails.inertia.sharedProps = {}
       sails.inertia.sharedViewData = {}
-      sails.on('router:before', function routerBefore() {
-        routesToBindInertiaTo.forEach(function iterator(routeAddress) {
+      sails.on('router:before', function () {
+        routesToBindInertiaTo.forEach(function (routeAddress) {
           sails.router.bind(routeAddress, inertia(hook))
         })
       })
     },
 
-    share: (key, value = null) => (sails.inertia.sharedProps[key] = value),
+    /**
+     * Share a property globally
+     * @param {string} key - The key of the property
+     * @param {*} value - The value of the property
+     */
+    share: function (key, value = null) {
+      return (sails.inertia.sharedProps[key] = value)
+    },
 
-    getShared: (key = null) =>
-      sails.inertia.sharedProps[key] ?? sails.inertia.sharedProps,
+    /**
+     * Get shared properties
+     * @param {string|null} key - The key of the property to get, or null to get all
+     * @returns {*} - The shared property or all shared properties
+     */
+    getShared: function (key = null) {
+      return sails.inertia.sharedProps[key] ?? sails.inertia.sharedProps
+    },
 
-    flushShared: (key) => {
-      key
+    /**
+     * Flush shared properties
+     * @param {string|null} key - The key of the property to flush, or null to flush all
+     */
+    flushShared: function (key) {
+      return key
         ? delete sails.inertia.sharedProps[key]
         : (sails.inertia.sharedProps = {})
     },
 
-    viewData: (key, value) => (sails.inertia.sharedViewData[key] = value),
+    /**
+     * Add view data
+     * @param {string} key - The key of the view data
+     * @param {*} value - The value of the view data
+     */
+    viewData: function (key, value) {
+      return (sails.inertia.sharedViewData[key] = value)
+    },
 
-    getViewData: (key) =>
-      sails.inertia.sharedViewData[key] ?? sails.inertia.sharedViewData
+    /**
+     * Get view data
+     * @param {string} key - The key of the view data to get
+     * @returns {*} - The view data
+     */
+    getViewData: function (key) {
+      return sails.inertia.sharedViewData[key] ?? sails.inertia.sharedViewData
+    },
+
+    /**
+     * Create a deferred prop
+     * This allows you to load certain page data after the initial render.
+     * @docs https://docs.sailscasts.com/boring-stack/deferred-props
+     * @param {Function} cb - The callback function to execute
+     * @param {string} group - The group name
+     * @returns {DeferProp} - The deferred prop
+     */
+    defer: function (cb, group = 'default') {
+      return new DeferProp(cb, group)
+    },
+
+    /**
+     * Render the response
+     * @param {Object} req - The request object
+     * @param {Object} res - The response object
+     * @param {Object} data - The data to render
+     * @returns {*} - The rendered response
+     */
+    render: function (req, res, data) {
+      return render(req, res, data)
+    }
   }
 }
