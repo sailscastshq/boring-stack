f9133df4151e1829da773c35c9611772ca2a7b65
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-02-11 22:15:50 +0100

    feat(inertia-sails): extract building of Inertia page object to a module

---
+++ packages/inertia-sails/private/helpers/build-page-object.js
@@ -0,0 +1,27 @@
+const pickPropsToResolve = require('../props/pick-props-to-resolve')
+const resolveDeferredProps = require('../props/resolve-deferred-props')
+const resolveMergeProps = require('../props/resolve-merge-props')
+const resolvePageProps = require('../props/resolve-page-props')
+
+module.exports = async function buildPageObject(req, component, pageProps) {
+  const sails = req._sails
+  let url = req.url || req.originalUrl
+  const assetVersion = sails.config.inertia.version
+  const currentVersion =
+    typeof assetVersion === 'function' ? assetVersion() : assetVersion
+  const propsToResolve = pickPropsToResolve(req, component, {
+    ...sails.inertia.sharedProps,
+    ...pageProps
+  })
+
+  return {
+    component,
+    url,
+    version: currentVersion,
+    props: await resolvePageProps(propsToResolve),
+    clearHistory: sails.config.inertia.history.clear,
+    encryptHistory: sails.config.inertia.history.encrypt,
+    ...resolveMergeProps(req, pageProps),
+    ...resolveDeferredProps(req, component, pageProps)
+  }
+}
