1bbfee32cf90639868621f7c21a8fc4de0dec8bb
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-09-08 20:09:53 +0100

    feat(ascent-react): implement has-partially-logged-in policy for 2FA verification routes

--- templates/ascent-react/config/policies.js
+++ templates/ascent-react/config/policies.js
@@ -16,6 +16,9 @@ module.exports.policies = {
    *                                                                          *
    ***************************************************************************/
   'auth/*': 'is-guest',
+  'auth/view-verify-2fa': 'has-partially-logged-in',
+  'auth/verify-2fa': 'has-partially-logged-in',
+  'auth/send-login-email-2fa': 'has-partially-logged-in',
   'user/*': 'is-authenticated',
   'dashboard/*': 'is-authenticated',
   'setting/*': 'is-authenticated'
