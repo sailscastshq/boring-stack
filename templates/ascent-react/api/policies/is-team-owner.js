3422fcf11a945d8349a542cc987257352d638379
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-09-10 19:14:20 +0100

    feat(ascent-react): add policy to check team ownership for logged-in users

---
+++ templates/ascent-react/api/policies/is-team-owner.js
@@ -0,0 +1,10 @@
+/**
+ * Policy to check if the logged-in user owns the specified team
+ */
+module.exports = function (req, res, proceed) {
+  const teamId = req.param('teamId')
+  if (req.session.teamId !== teamId) {
+    return res.forbidden()
+  }
+  return proceed()
+}
