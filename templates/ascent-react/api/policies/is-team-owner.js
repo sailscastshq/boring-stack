b2c3a73fed5f240230f866f252c014947f545048
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-09-10 21:24:26 +0100

    fix(is-team-owner): update equality check to use loose comparison for team ownership validation

--- templates/ascent-react/api/policies/is-team-owner.js
+++ templates/ascent-react/api/policies/is-team-owner.js
@@ -3,7 +3,7 @@
  */
 module.exports = function (req, res, proceed) {
   const teamId = req.param('teamId')
-  if (req.session.teamId !== teamId) {
+  if (req.session.teamId != teamId) {
     return res.forbidden()
   }
   return proceed()
