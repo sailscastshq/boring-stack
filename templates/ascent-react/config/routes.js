5e85ce88af1790f144180f9761f46912f01da18a
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-09-22 15:55:47 +0100

    feat(ascent-react): add route for sending email invites to team members

--- templates/ascent-react/config/routes.js
+++ templates/ascent-react/config/routes.js
@@ -77,6 +77,7 @@ module.exports.routes = {
   'POST /teams/:teamId/reset-invite-token': 'team/reset-invite-token',
   'POST /teams/:teamId/invite-response': 'team/invite-response',
   'POST /teams/:teamId/toggle-invite-link': 'team/toggle-invite-link',
+  'POST /teams/:teamId/send-email-invite': 'team/send-email-invite',
   'PATCH /teams/:teamId/domain-restrictions': 'team/set-domain-restrictions',
   'DELETE /teams/:teamId/remove-domain-restriction':
     'team/remove-domain-restriction',
