20d45ea06ee2621354b4217ce6197306ea18d27b
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-10-16 23:25:10 +0100

    refactor(ascent-react): remove slug attribute and related logic from Team model

--- templates/ascent-react/api/models/Team.js
+++ templates/ascent-react/api/models/Team.js
@@ -9,14 +9,6 @@ module.exports = {
       description:
         'The display name of the team that users see in the interface'
     },
-    slug: {
-      type: 'string',
-      regex: /^[a-z0-9-]+$/,
-      maxLength: 50,
-      example: 'my-team',
-      description:
-        'URL-friendly identifier for the team, automatically generated from name'
-    },
     settings: {
       type: 'json',
       defaultsTo: {},
@@ -63,7 +55,6 @@ module.exports = {
     return {
       id: this.id,
       name: this.name,
-      slug: this.slug,
       settings: this.settings,
       inviteToken: this.inviteToken,
       inviteLink: this.inviteLink,
@@ -80,46 +71,6 @@ module.exports = {
       valuesToSet.inviteToken = await sails.helpers.strings.random(
         'url-friendly'
       )
-    }
-    if (!valuesToSet.slug && valuesToSet.name) {
-      valuesToSet.slug = await sails.helpers.strings.toSlug(valuesToSet.name)
-
-      let baseSlug = valuesToSet.slug
-      let counter = 1
-      let slugExists = true
-
-      while (slugExists) {
-        const existingTeam = await Team.findOne({ slug: valuesToSet.slug })
-        if (!existingTeam) {
-          slugExists = false
-        } else {
-          valuesToSet.slug = `${baseSlug}-${counter}`
-          counter++
-        }
-      }
-    }
-    return proceed()
-  },
-  beforeUpdate: async function (valuesToSet, proceed) {
-    if (valuesToSet.name && !valuesToSet.slug) {
-      valuesToSet.slug = await sails.helpers.strings.toSlug(valuesToSet.name)
-
-      let baseSlug = valuesToSet.slug
-      let counter = 1
-      let slugExists = true
-
-      while (slugExists) {
-        const existingTeam = await Team.findOne({
-          slug: valuesToSet.slug,
-          id: { '!=': this.id }
-        })
-        if (!existingTeam) {
-          slugExists = false
-        } else {
-          valuesToSet.slug = `${baseSlug}-${counter}`
-          counter++
-        }
-      }
     }
     return proceed()
   }
