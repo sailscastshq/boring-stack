f352a6756754cac6c91427a0bb7fdc38493660f7
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-10-22 04:16:29 +0100

    fix(ascent-react): ensure shared state is flushed during user logout

--- templates/ascent-react/api/controllers/user/logout.js
+++ templates/ascent-react/api/controllers/user/logout.js
@@ -13,6 +13,9 @@ module.exports = {
 
   fn: async function () {
     sails.inertia.flushShared('loggedInUser')
+    sails.inertia.flushShared('teams')
+    sails.inertia.flushShared('currentTeam')
+
     delete this.req.session.userId
     return '/login'
   }
