3448a0edecc323b537b8abb40c9e256a699c2563
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-10-24 01:27:15 +0100

    fix(ascent-vue): adjust border opacity for header for improved visibility

--- templates/ascent-vue/assets/js/layouts/AppLayout.vue
+++ templates/ascent-vue/assets/js/layouts/AppLayout.vue
@@ -16,7 +16,7 @@ useFlashToast()
     class="bg-linear-to-b from-brand-50/10 flex min-h-screen flex-col to-[#F9FAFB] dark:from-gray-900 dark:to-gray-950"
   >
     <header
-      class="sticky top-0 z-50 border-b border-gray-100/40 bg-white/90 backdrop-blur-md dark:border-gray-800/40 dark:bg-gray-900/90"
+      class="sticky top-0 z-50 border-b border-gray-100 border-opacity-10 bg-white/90 backdrop-blur-md dark:border-gray-800 dark:border-opacity-40 dark:bg-gray-900/90"
     >
       <nav
         class="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 md:px-8"
