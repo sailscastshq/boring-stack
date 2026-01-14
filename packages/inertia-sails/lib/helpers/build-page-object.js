d63bfde8fa3657a998dd6fe666bd45007d47fb0d
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2026-01-15 00:44:49 +0100

    feat(inertia-sails): enhance buildPageObject function
    with improved prop handling and flash data integration

--- packages/inertia-sails/lib/helpers/build-page-object.js
+++ packages/inertia-sails/lib/helpers/build-page-object.js
@@ -4,6 +4,32 @@ const resolveMergeProps = require('../props/resolve-merge-props')
 const { resolveOncePropsMetadata } = require('../props/resolve-once-props')
 const resolvePageProps = require('../props/resolve-page-props')
 
+/**
+ * @typedef {Object} InertiaPageObject
+ * @property {string} component - The component name to render
+ * @property {string} url - The current URL
+ * @property {string|number} version - Asset version for cache busting
+ * @property {Object.<string, *>} props - Resolved page props
+ * @property {boolean} clearHistory - Whether to clear browser history
+ * @property {boolean} encryptHistory - Whether to encrypt history state
+ * @property {string[]} [mergeProps] - Props that should be merged on client
+ * @property {string[]} [deepMergeProps] - Props that should be deep merged
+ * @property {Object.<string, string[]>} [deferredProps] - Deferred props by group
+ * @property {Object.<string, *>} [onceProps] - Once-prop metadata
+ * @property {Object.<string, *>} [flash] - Flash data (not persisted in history)
+ */
+
+/**
+ * Build the Inertia page object for a response.
+ *
+ * Combines shared props with page props, resolves special prop types,
+ * and builds the complete page object sent to the client.
+ *
+ * @param {Object} req - Express/Sails request object
+ * @param {string} component - The component name to render
+ * @param {Object.<string, *>} pageProps - Props specific to this page
+ * @returns {Promise<InertiaPageObject>} - The complete page object
+ */
 module.exports = async function buildPageObject(req, component, pageProps) {
   const sails = req._sails
   let url = req.url || req.originalUrl
@@ -26,19 +52,17 @@ module.exports = async function buildPageObject(req, component, pageProps) {
     props: await resolvePageProps(propsToResolve),
     clearHistory: sails.inertia.shouldClearHistory,
     encryptHistory: sails.inertia.shouldEncryptHistory,
-    ...resolveMergeProps(req, pageProps),
-    ...resolveDeferredProps(req, component, pageProps),
+    ...resolveMergeProps(req, allProps),
+    ...resolveDeferredProps(req, component, allProps),
     ...resolveOncePropsMetadata(allProps)
   }
 
-  // Add flash data if present (not persisted in browser history)
-  if (
-    sails.inertia._flashData &&
-    Object.keys(sails.inertia._flashData).length > 0
-  ) {
-    page.flash = sails.inertia._flashData
-    // Clear flash data after adding to response
-    sails.inertia._flashData = {}
+  // Consume flash data from session and add to props
+  // Flash data is included in props.flash so it's accessible via usePage().props.flash
+  // Note: Unlike regular props, flash data should NOT be persisted in browser history
+  const flash = sails.inertia.consumeFlash(req)
+  if (Object.keys(flash).length > 0) {
+    page.props.flash = flash
   }
 
   return page
