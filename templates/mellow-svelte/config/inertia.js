f841122309d806dc4f120c2f2489a689c00620ab
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2026-01-15 02:50:41 +0100

    feat(inertia-sails): simplify Inertia configuration by removing unused comments
    and updating documentation links

--- templates/mellow-svelte/config/inertia.js
+++ templates/mellow-svelte/config/inertia.js
@@ -2,18 +2,8 @@
  * Inertia configuration
  * (sails.config.inertia)
  *
- * Use the settings below to configure session integration in your app.
- *
  * For more information on Inertia configuration, visit:
- * https://inertia-sails.sailscasts.com
+ * https://docs.sailscasts.com/inertia-sails/
  */
 
-module.exports.inertia = {
-  /**
-   * https://inertiajs.com/asset-versioning
-   * You can pass a string, number that changes when your assets change
-   *  or a function that returns the said string, number.
-   * e.g 4 or () => 4
-   */
-  // version: 1,
-}
+module.exports.inertia = {}
